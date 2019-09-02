dea883534cc1c59f461976e1f3c5ef2518e86e18
Hunter <abating.xiahaiquan@fenda.com>
2019-09-02 14:58:47 +0800

    Initial Commit

---
+++ HQHeartRateWaveform/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HQHeartRateWaveform
+//
+//  Created by HaiQuan on 2019/9/2.
+//  Copyright © 2019 HaiQuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
